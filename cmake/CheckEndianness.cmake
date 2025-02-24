ab6f2f2b3e355d18b615ac75dd98bbcfd303a620
Mark Gillard <marzer_@hotmail.com>
2025-02-24 19:50:38 +0200

    Fix breakage when used as a subproject

--- cmake/CheckEndianness.cmake
+++ cmake/CheckEndianness.cmake
@@ -5,8 +5,8 @@ macro(define_python_endianness_macros)
 
   # Test for float endianness
   try_run(RESULT_VAR COMPILE_VAR
-    ${CMAKE_BINARY_DIR}/check_float_endianness_result
-    ${CMAKE_SOURCE_DIR}/cmake/check_float_endianness.c
+    ${CMAKE_CURRENT_BINARY_DIR}/check_float_endianness_result
+    ${PROJECT_SOURCE_DIR}/cmake/check_float_endianness.c
   )
   if(RESULT_VAR EQUAL 1)
     message(STATUS "System uses big-endian floating point format")
