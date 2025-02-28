7b05ce2e38a0724fb05d06e1d4763fb7e5ce8607
Mark Gillard <marzer_@hotmail.com>
2025-02-28 18:28:48 +0200

    fix some unconditional try_run() calls (#40)

--- cmake/CheckEndianness.cmake
+++ cmake/CheckEndianness.cmake
@@ -1,18 +1,20 @@
 include(TestBigEndian)
 
 macro(define_python_endianness_macros)
-  TEST_BIG_ENDIAN(WORDS_BIGENDIAN)
+  if (NOT DEFINED FLOAT_WORDS_BIGENDIAN)
+    TEST_BIG_ENDIAN(WORDS_BIGENDIAN)
 
-  # Test for float endianness
-  try_run(RESULT_VAR COMPILE_VAR
-    ${CMAKE_CURRENT_BINARY_DIR}/check_float_endianness_result
-    ${PROJECT_SOURCE_DIR}/cmake/check_float_endianness.c
-  )
-  if(RESULT_VAR EQUAL 1)
-    message(STATUS "System uses big-endian floating point format")
-    set(FLOAT_WORDS_BIGENDIAN 1)
-  else()
-    message(STATUS "System uses little-endian floating point format")
-    set(FLOAT_WORDS_BIGENDIAN 0)
+    # Test for float endianness
+    try_run(FLOAT_WORDS_BIGENDIAN_RESULT_VAR FLOAT_WORDS_BIGENDIAN_COMPILE_VAR
+      ${CMAKE_CURRENT_BINARY_DIR}/check_float_endianness_result
+      ${PROJECT_SOURCE_DIR}/cmake/check_float_endianness.c
+    )
+    if(FLOAT_WORDS_BIGENDIAN_RESULT_VAR EQUAL 1)
+      message(STATUS "System uses big-endian floating point format")
+      set(FLOAT_WORDS_BIGENDIAN 1)
+    else()
+      message(STATUS "System uses little-endian floating point format")
+      set(FLOAT_WORDS_BIGENDIAN 0)
+    endif()
   endif()
 endmacro()
